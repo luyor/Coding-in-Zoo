e83caa29841881661d8a36a52b1eeb17e35f9549
Ayke <lyaule@gmail.com>
2014-12-20 15:52:33 +0800

    填补了产生敌人的函数newenemy（未完成）

--- GameDesign/design.h
+++ GameDesign/design.h
@@ -24,11 +24,7 @@ public:
     Design();
     //NewEnemy is used to register new enemy in game engine, using game.EnemyRegister()
     //background_position started with 0
-    void NewEnemy(Game& game, double backgrond_position)
-    {
-        if (!EnemyLists[CurrentEnemyNumber]) return;
-        while (EnemyLists[CurrentEnemyNumber]->OccurPosition})
-    }
+    void NewEnemy(Game& game, double backgrond_position);
 
     void Reset()//reset level
     {
@@ -42,19 +38,33 @@ public:
     }
 
 private:
-    Enemy EnemyType1();//generate a "type 1" enemy
-    Enemy EnemyType2();//generate a "type 2" enemy
-    Enemy EnemyType3();//generate a "type 3" enemy
-    Enemy EnemyType4();//generate a "type 4" enemy
     int CurrentEnemyNumber = 0;
 
     Pointer EnemyLists[N_ENEMY];
 };
 
-
-void Design::NewEnemy(Game &game, double backgrond_position)
+void Design::NewEnemy(Game &game, double background_position)
 {
-
+    if (!EnemyLists[CurrentEnemyNumber]) return;
+    while (EnemyLists[CurrentEnemyNumber]->OccurPosition < background_position)
+    {
+        switch (EnemyLists[CurrentEnemyNumber]->EmemyType) {
+            case 0:
+                break;
+            case 1:
+                Enemy1 x();
+                break;
+            case 2:
+                Enemy2 x();
+                break;
+            case 3:
+                Enemy3 x();
+                break;
+            case 4:
+                Enemy4 x();
+                break;
+        }
+    }
 }
 
 #endif // DESIGN_H
