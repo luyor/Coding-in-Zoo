63ef89fd33fed4aa9855947e3167618a48f3cd80
Ayke <lyaule@gmail.com>
2014-12-20 12:17:14 +0800

    design.h添加了不同的敌人类型

--- GameDesign/design.h
+++ GameDesign/design.h
@@ -1,7 +1,12 @@
 #ifndef DESIGN_H
 #define DESIGN_H
 #define N_ENEMY 1000
+
+#include "../GameEngine/game.h"
+#include "../GameEngine/enemy.h"
 class Game;
+
+class Enemy;
 
 class Design
 {
@@ -12,13 +17,19 @@ public:
     void NewEnemy(Game& game, double backgrond_position);
     void Reset();//reset level
 private:
-    int Current
+    Enemy EnemyType1();//generate a "type 1" enemy
+    Enemy EnemyType2();//generate a "type 2" enemy
+    Enemy EnemyType3();//generate a "type 3" enemy
+    Enemy EnemyType4();//generate a "type 4" enemy
+    int CurrentEnemyNumber = 0;
     struct MeetingPoint{
         int EmemyType;
         double OccurPosition;
     };
     struct MeetingPoint EnemyLists[N_ENEMY];
 };
+
+void EnemyType1(void)
 
 void Design::NewEnemy(Game &game, double backgrond_position)
 {
