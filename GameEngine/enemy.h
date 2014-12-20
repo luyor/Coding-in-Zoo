0a13641bcf47c92b7e9467cd26f62c079f4c8b00
Ayke <lyaule@gmail.com>
2014-12-20 16:42:32 +0800

    给Enemy.h加了注释；实现了Newenemy()

--- GameEngine/enemy.h
+++ GameEngine/enemy.h
@@ -8,6 +8,14 @@ class Game;
 class Enemy:public FlyingObject
 {
 public:
+    //point v is the velocity
+    //point p is the position
+    //angle0 is -pi/2 for heading down
+    //vector<Circle>& hit_point0 is for collision
+    //Graphic grphic0 is for displaying
+    //health0 is the health when it was born
+    //HIT_SCORE0 is the score you get when you hit this enemy
+    //DESTROY_SCORE is the score you get when this enemy get destroyed
     Enemy(Point v,Point p,double angle0,vector<Circle>& hit_point0,
           Graphic graphic0,int health0,int HIT_SCORE0,int DESTROY_SCORE0);
     //do damge and return score
