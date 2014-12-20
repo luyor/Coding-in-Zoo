f8daa6fd8d642596fa25112eee18294ec478bd0b
Ayke <lyaule@gmail.com>
2014-12-20 12:18:06 +0800

    为enemy()添加了注释

--- GameEngine/enemy.h
+++ GameEngine/enemy.h
@@ -8,6 +8,14 @@ class Game;
 class Enemy:public FlyingObject
 {
 public:
+    //Point v is velocity
+    //Point p is position
+    //angle0 is set to be -pi/2 when heading down
+    //hit_point0 is used for collision
+    //graphic0 ***
+    //health0 is the "health" at very first beginning
+    //HIT_SCORE0 is the number of score you get when you hit it
+    //DESTROY_SCORE0 is the number of score you get when you destory it
     Enemy(Point v,Point p,double angle0,vector<Circle>& hit_point0,
           Graphic graphic0,int health0,int HIT_SCORE0,int DESTROY_SCORE0);
     //do damge and return score
@@ -17,11 +25,6 @@ private:
     int health;
     int HIT_SCORE;
     int DESTROY_SCORE;
-};
-
-class Enemy1:public Enemy
-{
-
 };
 
 #endif // ENEMY_H
