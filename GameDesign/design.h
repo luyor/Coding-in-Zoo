a80763d4a7c0a3d0b47a7f518d610157b63d6e58
Ayke <lyaule@gmail.com>
2014-12-20 13:17:10 +0800

    把多个类型修改成类，实现了MissionFinish()等函数

--- GameDesign/design.h
+++ GameDesign/design.h
@@ -8,28 +8,49 @@ class Game;
 
 class Enemy;
 
+struct MeetingPoint{
+    int EmemyType;
+    //Be careful! OccurPosition is not the position where the enemy would be born
+    //Instead, when the "background_position">"occurposition", this enemy should appear now
+    //the position where the enemy would born is DEFAULT_GAME_AREA_TOP_RIGHT
+    double OccurPosition;
+};
+
+typedef struct MeetingPoint *Pointer;
+
 class Design
 {
 public:
     Design();
     //NewEnemy is used to register new enemy in game engine, using game.EnemyRegister()
     //background_position started with 0
-    void NewEnemy(Game& game, double backgrond_position);
-    void Reset();//reset level
+    void NewEnemy(Game& game, double backgrond_position)
+    {
+        if (!EnemyLists[CurrentEnemyNumber]) return;
+        while (EnemyLists[CurrentEnemyNumber]->OccurPosition})
+    }
+
+    void Reset()//reset level
+    {
+        CurrentEnemyNumber = 0;
+    }
+
+    bool MissionFinish()
+    {
+        if (!EnemyLists[CurrentEnemyNumber]) return true;
+        else return false;
+    }
+
 private:
     Enemy EnemyType1();//generate a "type 1" enemy
     Enemy EnemyType2();//generate a "type 2" enemy
     Enemy EnemyType3();//generate a "type 3" enemy
     Enemy EnemyType4();//generate a "type 4" enemy
     int CurrentEnemyNumber = 0;
-    struct MeetingPoint{
-        int EmemyType;
-        double OccurPosition;
-    };
-    struct MeetingPoint EnemyLists[N_ENEMY];
+
+    Pointer EnemyLists[N_ENEMY];
 };
 
-void EnemyType1(void)
 
 void Design::NewEnemy(Game &game, double backgrond_position)
 {
